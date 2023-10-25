43944733c5f6efa83e38b0e171756b26ead8689a
Ike <dylanraspino264@gmail.com>
2023-10-25 20:49:26 +0000

    build--complete-user-works

--- backend/api/views.py
+++ backend/api/views.py
@@ -99,15 +99,31 @@ class ProductsList(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
    
+    # def post(self, request, format=None):
+    #     serializer = ProductSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
+
+class ProductCreateAPIView(APIView):
     def post(self, request, format=None):
+        user_id = request.data.get('user')
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
+        
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=user)  # Assign the current user to the product
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
 
 class ProductDetail(APIView):
